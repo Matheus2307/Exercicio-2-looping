5fa06b525741db83ad1ae73054b6f190a296ca8a
Matheus Silva <matheus161170@gmail.com>
2020-11-22 23:06:15 -0300

    Exercicio 2 looping

---
+++ ValoresPares_MatheusSilva/src/valorespares_matheussilva/ValoresPares_MatheusSilva.java
@@ -0,0 +1,21 @@
+
+package valorespares_matheussilva;
+
+
+public class ValoresPares_MatheusSilva {
+
+ 
+    public static void main(String[] args) {
+        int i=1000;
+     do
+     {
+      System.out.println(i);
+       i +=2;
+     }
+     while(i<=1500);
+     
+         
+         
+    }
+    
+}
